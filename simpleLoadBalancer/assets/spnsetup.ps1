7aa9175b7856279e6d01284245bcbe0c11bb6951
Premanshu Mukherji <90206846+prem-mukherji@users.noreply.github.com>
2024-05-13 19:31:48 +0530

    Update spnsetup.ps1

--- simpleLoadBalancer/assets/spnsetup.ps1
+++ simpleLoadBalancer/assets/spnsetup.ps1
@@ -2,7 +2,7 @@
 az login
 
 # Create an SPN, only if you are Owner of Subscription or have User Access Management role
-az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/df43b9ea-cec5-46f2-ace2-622430cfc26d" --display-name "spn-prem-cpl"
+az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/<subscription-guid>" --display-name "spn-prem-cpl"
 
 # Alternatively, create an SPN via Admiral and provide the Contributor role.
 # For this case, use service-principal spn-prem-cpl (appid: eec83b7b-fb97-4af5-a989-ffcd18e8df9d)
