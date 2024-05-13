09520bfd078166a54b08af327907b6e47da1b148
Premanshu Mukherji <90206846+prem-mukherji@users.noreply.github.com>
2024-05-13 19:42:49 +0530

    Update provider.tf

--- privateLink/src/provider.tf
+++ privateLink/src/provider.tf
@@ -14,6 +14,6 @@ provider "azurerm" {
   
   subscription_id   = "df43b9ea-cec5-46f2-ace2-622430cfc26d"
   tenant_id         = "05d75c05-fa1a-42e7-9cf1-eb416c396f2d"
-  client_id         = "eec83b7b-fb97-4af5-a989-ffcd18e8df9d"
-  client_secret     = "rIt8Q~DAU-ALHl30UIyqG~dVtLz~qeMdSTYVGdm4"
+  client_id         = "eec83b7b-<rest-guid>"
+  client_secret     = "<secret-text>"
 }
